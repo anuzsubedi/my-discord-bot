96044e22c8274d689de7b67b1f229c474169669e
anuzsubedi <anuzsubedi@hotmail.com>
2024-08-20 00:33:33 -0400

    feat: Add admin cog and dmrole command

---
+++ cogs/admin.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Admin Cog is ready")
+
+    @app_commands.command(
+        name="dmrole", description="Send a DM to users with a specific role."
+    )
+    async def dmrole(self, ctx: discord.Interaction, role: discord.Role, message: str):
+        try:
+            members = role.members
+            for member in members:
+                await member.send(message)
+            await ctx.response.send_message(
+                f"Sent message to {len(members)} members.", ephemeral=True
+            )
+        except Exception as e:
+            print(f"\n++++++++++++\n")
+            print(e)
+
+
+# Add the cog to the bot
+async def setup(bot):
+    await bot.add_cog(Admin(bot))
