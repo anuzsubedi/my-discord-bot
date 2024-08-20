869a6055dfa681519b5a2f8c16654140cc17dc49
anuzsubedi <anuzsubedi@hotmail.com>
2024-08-20 01:07:53 -0400

    refactor: Add detailed member join logging with account age and new user flag

--- cogs/logger.py
+++ cogs/logger.py
@@ -16,22 +16,78 @@ class Logger(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
+        isNewUser = False
+        # Retrieve the channels from the configuration
         join_leave_channel = self.bot.get_channel(
             config["configuration"]["join-leave-channel"]
         )
+        member_details_channel = self.bot.get_channel(
+            config["configuration"]["member-detail-log-channel"]
+        )
 
+        # Ensure the channels are valid
+        if join_leave_channel is None or member_details_channel is None:
+            return
+
+        # Calculate account age
+        account_creation = member.created_at.astimezone(pytz.UTC)
+        now = datetime.now(pytz.UTC)
+        account_age_timedelta = now - account_creation
+        account_age_days = account_age_timedelta.days
+        account_age_hours = int(account_age_timedelta.total_seconds() // 3600) % 24
+        account_age_minutes = int(account_age_timedelta.total_seconds() // 60) % 60
+        if account_age_days < 30:
+            isNewUser = True
+        if account_age_days >= 365:
+            years = account_age_days // 365
+            days_remaining = account_age_days % 365
+            account_age = f"{years} years, {days_remaining} days."
+        elif account_age_days > 1:
+            account_age = f"{account_age_days} days, {account_age_hours} hours, {account_age_minutes} minutes."
+        elif account_age_days == 1:
+            account_age = (
+                f"1 day, {account_age_hours} hours, {account_age_minutes} minutes."
+            )
+        elif account_age_hours > 0:
+            account_age = f"{account_age_hours} hours, {account_age_minutes} minutes."
+        else:
+            account_age = f"{account_age_minutes} minutes."
+
+        # Embed for the general channel
         title = "Member Joined"
         description = f"Welcome {member.mention} to the server!\nPlease familiarize yourself with the rules and enjoy your stay!"
         joined_at = member.joined_at.strftime("%Y-%m-%d %H:%M")
+
         embed = discord.Embed(
             title=title,
             description=description,
             color=discord.Color.green(),
         )
-
         embed.set_footer(text=f"Joined at: {joined_at}")
 
+        # Embed for the member details log
+        detailEmbed = discord.Embed(
+            title="Member Details",
+            description=f"New User {member.mention} has joined the server.",
+            color=discord.Color.red() if isNewUser else discord.Color.green(),
+        )
+        detailEmbed.add_field(
+            name="Account Created",
+            value=account_creation.strftime("%Y-%m-%d %H:%M"),
+            inline=False,
+        )
+        detailEmbed.add_field(name="Account Age", value=account_age, inline=False)
+        detailEmbed.add_field(name="Member Joined", value=joined_at, inline=False)
+
+        (
+            detailEmbed.add_field(name="New User", value="Yes", inline=False)
+            if isNewUser
+            else None
+        )
+
+        # Send the embeds to the appropriate channels
         await join_leave_channel.send(embed=embed)
+        await member_details_channel.send(embed=detailEmbed)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
