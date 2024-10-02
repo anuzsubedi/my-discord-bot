dc50d5d9b6005cf235317e3644752b8bc7258b12
anuzsubedi <anuzsubedi@hotmail.com>
2024-10-01 21:35:05 -0400

    chore: add function to check migration and create tables if necessary

--- utils/dbmanager.py
+++ utils/dbmanager.py
@@ -1,5 +1,4 @@
 import yaml
-
 import mysql.connector
 
 
@@ -26,3 +25,54 @@ class DatabaseManager:
             print("Connected to MySQL database successfully!")
         except mysql.connector.Error as error:
             print("Error connecting to MySQL database:", error)
+
+    def check_migration(self):
+        self.connect_to_mysql()
+        self.check_required_tables()
+
+    def check_required_tables(self):
+        self.cursor.execute("SHOW TABLES")
+        tables = {table[0] for table in self.cursor.fetchall()}
+
+        if "serverlist" not in tables:
+            self.create_serverlist_table()
+            print("Serverlist table created successfully!")
+
+        if "channels" not in tables:
+            self.create_channels_table()
+            print("Channels table created successfully!")
+
+    def create_serverlist_table(self):
+        create_table_query = """
+        CREATE TABLE serverlist (
+            JoinID INTEGER PRIMARY KEY AUTO_INCREMENT,
+            GuildID BIGINT NOT NULL UNIQUE,
+            FirstConnectedDate DATETIME NOT NULL,
+            RecentConnectedDate DATETIME NOT NULL
+        );
+        """
+        self.cursor.execute(create_table_query)
+        self.db.commit()
+
+    def create_channels_table(self):
+        create_table_query = """
+        CREATE TABLE channels (
+            GuildID BIGINT PRIMARY KEY,
+            AdminRoleID BIGINT NOT NULL,
+            ModeratorRoleID BIGINT NOT NULL,
+            ChangeLogChannelID BIGINT NOT NULL,
+            AnnouncementChannelID BIGINT NOT NULL,
+            LeaveJoinChannelID BIGINT NOT NULL,
+            AdvancedLeaveJoinChannelID BIGINT NOT NULL,
+            FOREIGN KEY (GuildID) REFERENCES serverlist (GuildID)
+        );
+        """
+        self.cursor.execute(create_table_query)
+        self.db.commit()
+
+    def close_connection(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.db:
+            self.db.close()
+        print("Database connection closed.")
