24a4b6718b2ffd8e6a125a01278f23e5c6f7791a
anuzsubedi <anuzsubedi@hotmail.com>
2024-10-06 03:05:40 -0400

    refactor: Remove commented out code in configuration.py

--- cogs/configuration.py
+++ cogs/configuration.py
@@ -3,23 +3,6 @@ from discord.ext import commands
 from utils.dbmanager import DatabaseManager as db
 from discord import app_commands
 
-# @app_commands.command(
-#         name="testcommand", description="A command to test logging of messaageID"
-#     )
-#     async def test_command(
-#         self, ctx: discord.Interaction, message: str, channel: discord.TextChannel
-#     ):
-#         try:
-#             sentMessage = await channel.send(message)
-#             await sentMessage.add_reaction("🔔")
-#             await ctx.response.send_message(
-#                 f"Messaage sent to {channel.mention}. Message ID: {sentMessage.id}",
-#                 ephemeral=True,
-#             )
-#         except Exception as e:
-#             print(f"\n++++++++++++\n")
-#             print(e)
-
 
 class Configuration(commands.Cog):
     def __init__(self, bot):
@@ -32,10 +15,6 @@ class Configuration(commands.Cog):
         self, interaction: discord.Interaction, channel: discord.TextChannel
     ):
         try:
-            # db_manager = db()
-            # db_manager.connect_to_mysql()
-
-            # db_manager.close_connection()
             await interaction.response.send_message(
                 f"Announcement channel set to {channel.mention}.", ephemeral=True
             )
